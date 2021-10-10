f74ebc74534fdfaf8d06d945d5a9ce83df808b10
nishi-2400 <a7w21087bump@yahoo.co.jp>
2021-10-10 16:40:47 +0900

    study

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
+from pprint import pprint
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
-    return 'Welcome to Flask World!'
+    return render_template('index.html', title="Index with Jinja!", msg="Jinja template")
+
+
+@app.route('/',  methods=['POST'])
+def form():
+    ck = request.form.get('check')
+    rd = request.form.get('radio')
+    sel = request.form.getlist('sel')
+
+    pprint(ck)
+    return render_template('index.html', title="FORM", msg=[ck, rd, sel])
 
 
 if __name__ == '__main__':
