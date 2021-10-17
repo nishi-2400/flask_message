43ec7311098bcb2e634128f7dd7dff67db474fc7
nishi-2400 <a7w21087bump@yahoo.co.jp>
2021-10-17 20:20:52 +0900

    メッセージGET/POSTのSPA

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, jsonify
+import pickle
 from pprint import pprint
 
 app = Flask(__name__)
@@ -9,18 +10,80 @@ app.secret_key = b'radam string...'
 member_data = {}
 message_data = []
 
+member_data_file = 'member_data.dat'
+message_data_file = 'message_data.dat'
+
+# load member file
+try:
+    with open(member_data_file, "rb") as f:
+        list = pickle.load(f)
+        if list != None:
+            member_data = list
+except:
+    pass
+
+# load message file
+try:
+    with open(message_data_file, "rb") as f:
+        list = pickle.load(f)
+        if list != None:
+            message_data = list
+except:
+    pass
+
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', title="Index", message="*Vue.js")
-# def index():
-#     # グローバル変数
-#     global message_data
-#     if 'login' in session and session['login']:
-#         msg = 'Login id: ' + session['id']
-#         return render_template('message.html', title='Messages', messages=msg, data=message_data)
-#     else:
-#         return redirect('/login')
+    global message_data
+    return render_template('message.html', login=False, title="Messages", message="not logined", data="message_data")
+
+
+@app.route('/post', methods=['POST'])
+def posMsg():
+    global message_data
+    id = request.form.get('id')
+    msg = request.form.get('comment')
+    message_data.append((id, msg))
+
+    # 25件以上でメッセージ削除
+    if len(message_data) > 25:
+        message_data.pop(0)
+
+    try:
+        with open(message_data_file, 'wb') as f:
+            # ファイルにオブジェクトを保存する
+            pickle.dump(message_data, f)
+    except:
+        pass
+    return 'True'
+
+
+@app.route('/messages', methods=['GET'])
+def getMsg():
+    global message_data
+    return jsonify(message_data)
+
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    global member_data, message_data
+    id = request.form.get('id')
+    pswd = request.form.get('pass')
+    if id in member_data:
+        if pswd == member_data[id]:
+            flg = 'True'
+        else:
+            flg = 'False'
+    else:
+        member_data[id] = pswd
+        flg = 'True'
+
+        try:
+            with open(member_data_file, 'wb') as f:
+                pickle.dump(member_data, f)
+        except:
+            pass
+    return flg
 
 
 @app.route('/', methods=['POST'])
@@ -35,27 +98,6 @@ def form():
 @app.route('/login', methods=['GET'])
 def login():
     return render_template('login.html', title='Login', err=False, message='IDとパスワード入力：', id='')
-
-
-@app.route('/login', methods=['POST'])
-def login_post():
-    global member_data
-    id = request.form.get('id')
-    pswd = request.form.get('pass')
-    if id in member_data:
-        if pswd == member_data[id]:
-            session['login'] = True
-        else:
-            session['login'] = False
-    else:
-        member_data[id] = pswd
-        session['login'] = True
-
-    session['id'] = id
-    if session['login']:
-        return redirect('/')
-    else:
-        return render_template('login.html', title='Login', err=False, message='パスワードが違います', id=id)
 
 
 @app.route('/logout', methods=['GET'])
